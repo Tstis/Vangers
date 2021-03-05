659ca437260a5d0e0c9df1166fa6784f5da5a965
Ілля Махнотов <70032794+Tstis@users.noreply.github.com>
2021-03-05 18:28:39 +0300

    PortStrID on INet Host screen

--- src/iscreen/iextern.cpp
+++ src/iscreen/iextern.cpp
@@ -747,7 +747,7 @@ void iPrepareOptions(void)
 	iScrOpt[iPROXY_SERVER] = new iScreenOption(iSTRING,0,"INet Host screen","ProxyServerName");
 	iScrOpt[iPROXY_PORT] = new iScreenOption(iSTRING,0,"INet Host screen","ProxyPortNum");
 
-	iScrOpt[iSERVER_PORT] = new iScreenOption(iSTRING,0,"Port screen","PortStrID");
+	iScrOpt[iSERVER_PORT] = new iScreenOption(iSTRING,0,"INet Host screen","PortStrID");
 
 	iScrOpt[iPROXY_SERVER_STR] = new iScreenOption(iSTRING,0,"INet Host screen","ProxyServerStrID");
 	iScrOpt[iPROXY_SERVER_STR] -> flags |= iOPTION_NO_SAVE;
@@ -765,8 +765,8 @@ void iPrepareOptions(void)
 	iScrOpt[iFULLSCREEN] = new iScreenOption(iTRIGGER,0,"Graphics screen","FullScreenTrig");
 	((iTriggerObject *)iScrOpt[iFULLSCREEN]->objPtr)->callback = &iSetFullscreen;
 
-	iScrOpt[iAUTO_ACCELERATION] = new iScreenOption(iTRIGGER,0,"Controls screen","AutoAccelerationTrig");
 	iScrOpt[iCYCLE_DOBA] = new iScreenOption(iTRIGGER,0,"Graphics screen","CycleDobaTrig");
+	iScrOpt[iAUTO_ACCELERATION] = new iScreenOption(iTRIGGER,0,"Controls screen","AutoAccelerationTrig");
 	iPrepareControls();
 }
 
